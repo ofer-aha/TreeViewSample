e16e56ce793dccbafec0efc629a07721d6341cb1
ofer-aha <ofer.aha@gmail.com>
2021-10-17 21:01:27 +0300

    Add project files.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuTree = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menueAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuDel = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuRename = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.menuTree.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.ContextMenuStrip = this.menuTree;
+            this.treeView1.Location = new System.Drawing.Point(85, 77);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(230, 214);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterLabelEdit += new System.Windows.Forms.NodeLabelEditEventHandler(this.TreeView1_AfterLabelEdit);
+            this.treeView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TreeView1_MouseDown);
+            // 
+            // menuTree
+            // 
+            this.menuTree.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menueAdd,
+            this.menuDel,
+            this.menuRename});
+            this.menuTree.Name = "cMenu_Add";
+            this.menuTree.Size = new System.Drawing.Size(118, 70);
+            // 
+            // menueAdd
+            // 
+            this.menueAdd.Name = "menueAdd";
+            this.menueAdd.Size = new System.Drawing.Size(117, 22);
+            this.menueAdd.Text = "Add";
+            this.menueAdd.Click += new System.EventHandler(this.MenueAdd_Click);
+            // 
+            // menuDel
+            // 
+            this.menuDel.Name = "menuDel";
+            this.menuDel.Size = new System.Drawing.Size(117, 22);
+            this.menuDel.Text = "Del";
+            this.menuDel.Click += new System.EventHandler(this.MenuDel_Click);
+            // 
+            // menuRename
+            // 
+            this.menuRename.Name = "menuRename";
+            this.menuRename.Size = new System.Drawing.Size(117, 22);
+            this.menuRename.Text = "Rename";
+            this.menuRename.Click += new System.EventHandler(this.MenuRename_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(367, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(367, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "del";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(494, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 214);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(367, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "list";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(367, 239);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(362, 77);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Sample Data";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(367, 268);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Quit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 344);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuTree.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ContextMenuStrip menuTree;
+        private System.Windows.Forms.ToolStripMenuItem menueAdd;
+        private System.Windows.Forms.ToolStripMenuItem menuDel;
+        private System.Windows.Forms.ToolStripMenuItem menuRename;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private TreeNode mySelectedNode;
+
+        public Form1()
+        {
+            InitializeComponent();
+            treeView1.Nodes.Add("Default");
+            treeView1.HideSelection = false;
+            treeView1.PathSeparator = @"\";
+
+        }
+        private void AddNode()
+        {
+            mySelectedNode = treeView1.SelectedNode.Nodes.Add(" ");
+
+            treeView1.SelectedNode = mySelectedNode;
+            mySelectedNode.Expand();
+
+            RenameNode();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            AddNode();
+        }
+        private void DelNode()
+        {
+            TreeNode node = treeView1.SelectedNode;
+
+            if (node.Parent != null)
+            {
+                treeView1.SelectedNode.Nodes.Remove(treeView1.SelectedNode);
+            }
+            else
+            {
+                MessageBox.Show("Root node");
+            }
+
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            DelNode();
+        }
+
+
+        public void ListNodesPath(TreeNode oParentNode)
+        {
+            // Start recursion on all subnodes.
+            foreach (TreeNode oSubNode in oParentNode.Nodes)
+            {
+                if (oSubNode.Nodes.Count == 0)
+                {
+                    listBox1.Items.Add(oSubNode.FullPath);
+                }
+                ListNodesPath(oSubNode);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (TreeNode node in treeView1.Nodes)
+            {
+                if (node.Parent == null)
+                {
+                    ListNodesPath(node);
+                }
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            listBox1.Items.Clear();
+            treeView1.Nodes.Add("Default");
+            treeView1.SelectedNode = treeView1.Nodes[0];
+        }
+
+        private void MenueAdd_Click(object sender, EventArgs e)
+        {
+            AddNode();
+
+        }
+
+        private void MenuDel_Click(object sender, EventArgs e)
+        {
+            DelNode();
+        }
+
+        /// <summary>
+        /// Get the tree node under the mouse pointer and 
+        /// save it in the mySelectedNode variable.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TreeView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mySelectedNode = treeView1.GetNodeAt(e.X, e.Y);
+        }
+
+        private void MenuRename_Click(object sender, EventArgs e)
+        {
+            RenameNode();
+        }
+
+        private void RenameNode()
+        {
+            if (mySelectedNode != null && mySelectedNode.Parent != null)
+            {
+                treeView1.SelectedNode = mySelectedNode;
+                treeView1.LabelEdit = true;
+                if (!mySelectedNode.IsEditing)
+                {
+                    mySelectedNode.BeginEdit();
+                }
+            }
+            else
+            {
+                MessageBox.Show("No tree node selected or selected node is a root node.\n" +
+                   "Editing of root nodes is not allowed.", "Invalid selection");
+            }
+        }
+
+        private void TreeView1_AfterLabelEdit(object sender, NodeLabelEditEventArgs e)
+        {
+            if (e.Label != null)
+            {
+                if (e.Label.Length > 0)
+                {
+
+                    if (e.Label.IndexOfAny(new char[] { '@', '.', ',', '!' }) == -1)
+                    {
+                        // Stop editing without canceling the label change.
+                        e.Node.EndEdit(false);
+                    }
+                    else
+                    {
+                        /* Cancel the label edit action, inform the user, and
+                           place the node in edit mode again. */
+                        e.CancelEdit = true;
+                        MessageBox.Show("Invalid tree node label.\n" +
+                           "The invalid characters are: '@','.', ',', '!'",
+                           "Node Label Edit");
+                        e.Node.BeginEdit();
+                    }
+                }
+                else
+                {
+                    /* Cancel the label edit action, inform the user, and
+                       place the node in edit mode again. */
+                    e.CancelEdit = true;
+                    MessageBox.Show("Invalid tree node label.\nThe label cannot be blank",
+                       "Node Label Edit");
+                    e.Node.BeginEdit();
+                }
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode.Nodes.Add("A");
+            node.Nodes.Add("A1");
+            node.Nodes.Add("A2").Nodes.Add("A3");
+            node = treeView1.SelectedNode.Nodes.Add("B").Nodes.Add("B4");
+            node.Nodes.Add("B1");
+            node.Parent.Nodes.Add("B2");
+            node.Parent.Nodes.Add("B3");
+
+            treeView1.ExpandAll();
+
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        XmlDocument xmlDocument;
+
+        public void TreeViewToXml(TreeView treeView, String path)
+        {
+
+            xmlDocument = new XmlDocument();
+            TreeNodeCollection nodes = null;
+            foreach (TreeNode treeNode in treeView.Nodes)
+            {
+
+                xmlDocument.AppendChild(xmlDocument.CreateElement(treeNode.Text));
+                nodes = treeNode.Nodes;
+            }
+
+            XmlExport(xmlDocument.DocumentElement, nodes);
+            xmlDocument.Save(path);
+        }
+
+        private XmlNode XmlExport(XmlNode nodeElement, TreeNodeCollection treeNodeCollection)
+        {
+
+            XmlNode xmlNode = null;
+
+            foreach (TreeNode treeNode in treeNodeCollection)
+
+            {
+
+                xmlNode = xmlDocument.CreateElement(treeNode.Text);
+                string[] node = xmlNode.Name.Split(':');
+
+                if (node[0] == "ATTRIBUTE")
+                {
+
+                    if (node[0] != null && node[1] != null)
+                    {
+
+                        XmlAttribute newAttribute = xmlDocument.CreateAttribute(node[1]);
+                        nodeElement.Attributes.Append(newAttribute);
+                    }
+                }
+
+                else
+                {
+                    if (nodeElement != null) nodeElement.AppendChild(xmlNode);
+                }
+
+                if (treeNode.Nodes.Count > 0)
+                {
+                    XmlExport(xmlNode, treeNode.Nodes);
+                }
+            }
+            return xmlNode;
+
+        }
+    }
+}
